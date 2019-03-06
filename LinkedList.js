9fab49d4a673aebb92b5b338f09e3d98b70c3326
Yauhen Slizh <tyteishi@gmail.com>
2019-03-07 00:40:17 +0300

    Initial commit. Added base LinkedList class

---
+++ LinkedList.js
@@ -0,0 +1,34 @@
+class LinkedList {
+    constructor(value) {
+        this.head = null;
+        this.length = 0;
+        this.addToHead(value);
+    }
+
+    addToHead(value) {
+        const newNode = { value }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+}
+
+const linkedList = new LinkedList("First");
+
+console.log(linkedList);
+
+linkedList.addToHead('Second');
+
+console.log(linkedList);
+
+linkedList.addToHead('Third');
+
+console.log(linkedList);
+
+linkedList.addToHead('Forth');
+
+console.log(linkedList);
+
+
+
